96d6cb9922e3f67f9c8a3bc9bbb7968e9dd37dc9
sanv00 <sanagaraj2@gmail.com>
2025-10-01 10:05:18 -0700

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,10 @@
+#This is a python program
+#that prints out your full name, 
+#preferred pronouns, and two sentances
+#about your favorite movie 
+#and your favorite food.
+
+print("Full name: Sanvi Nagaraj")
+print("Pronouns: she/her") #optional
+print("My favorite movie is Hidden Figures, as it is very empowering.")
+print("My favorite food is pasta because of its versitility.")
