9faf651d5c878109d67a6c611009307df2d2103f
sanv00 <sanagaraj2@gmail.com>
2025-10-03 16:35:16 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 9	#define an integer x equal to 9
+y = 3 	#define an integer y equal to 3
+
+# Arithmetic Operators 
+print("x+y =", x+y) #addition
+print("x-y =", x-y) #subtraction
+print("x*y =", x*y) #multiplication
+print("x/y =", x/y) #division
+print("x%y =", x%y) #Modulus
+print("x**y =". x**y) #Exponential
+x = 9.191823
+print (x//y)		#Floor Division
+
+#Assignment Operators 
+x = 9 #set x = 9
+x += 3 #x = x + 3, x will equal the new value
+print("x = 9; x+=3; x = ", x)
+x = 9 #set x = 9
+x -=3 #x = x - 3, x will equal the new value
+print("x = 9; x-=3; x = ", x) 
+x *= 3 # x = x * 3, x will equal the new value
+print("x *= 3; x = ", x)
+x /= 3 #x = x / 3, x will equal the new value
+print ("x /=3; x = ", x)
+x **= 3 #x = x ** 3, x will equal the new number
+print("x **= 3; x = ", x)
+
+# Comparison Operators 
+x = 9
+x = 3
+print("x==y ?", x==y)	#True if x equals y, False otherwise
+print("x!=y ?", x!=y)	#True if x does not equal y, False otherwise
+print("x>y ?", x>y)		#True if x is greater than y, False otherwise 
+print("x<y ?", x<y)		#True if x is less than y, False otherwise
+print("x>=y ?", x>=y)	#True if x is greater than or equal to y, False otherwise
+print("x<=y ?", x<=y)	#True if x is less than or equal to y, False otherwise
